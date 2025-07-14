2c43de922b5e3df5af5b8a5767d14278c5d50b1b
vladkolchik <152319774+vladkolchik@users.noreply.github.com>
2025-07-14 23:35:58 +0300

    оптимизация модульной структуры и фиксы

--- keyboards/email_ui.py
+++ keyboards/email_ui.py
@@ -1,11 +1,5 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-def get_main_menu():
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="✉️ Отправка email", callback_data="email_mode")],
-        [InlineKeyboardButton(text="🆔 Получить ID", callback_data="id_mode")]
-    ])
-
 def get_email_menu():
     return InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="📎 Прикреплённые файлы", callback_data="show_attachments")],
